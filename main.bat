c935aa0b3055a6e8b20e9bc50334c7ebb9bc51fd
herme063 <herme063.dev@gmail.com>
2018-06-28 19:21:09 -0500

    Update main.bat
    skip existing thumbnails

--- main.bat
+++ main.bat
@@ -1,11 +1,12 @@
 @ECHO off >nul
 FOR /r %%F IN (*.mp4,*.avi,*.flv,*.mov) DO (
-	IF EXIST "%%~nF.gif" DEL "%%~nF.gif"
-	IF EXIST "%%~nF" RD /S /Q "%%~nF"
-	MD "%%~nF"
-	ffmpeg -i "%%~nxF" -vf "scale=320:-1,fps=1/60" "%%~nF/img_%%03d.jpg"
-	magick convert -delay 50 -loop 0 "%%~nF/img_*.jpg" "%%~nF.gif"
-	IF EXIST "%%~nF" RD /S /Q "%%~nF"
-	ECHO Generated "%%~nF.gif"...
+	IF NOT EXIST "%%~nF.gif" (
+		IF EXIST "%%~nF" RD /S /Q "%%~nF"
+		MD "%%~nF"
+		ffmpeg -i "%%~nxF" -vf "scale=320:-1,fps=1/60" "%%~nF/img_%%03d.jpg"
+		magick convert -delay 50 -loop 0 "%%~nF/img_*.jpg" "%%~nF.gif"
+		IF EXIST "%%~nF" RD /S /Q "%%~nF"
+		ECHO Generated "%%~nF.gif"...
+	)
 )
 PAUSE
